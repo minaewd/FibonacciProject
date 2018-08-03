878b88f511a95bc4012b6cc71ed4e82750735a78
Дмитрий <minaewdmitriy46@gmail.com>
2018-08-04 01:06:59 +0300

    Добавление метода получения массива чисел ряда Фибоначчи до заданного максимально возможного по значению члена. Изменен метод main

--- src/ru/minaevd/java/Fibonacci.java
+++ src/ru/minaevd/java/Fibonacci.java
@@ -8,9 +8,20 @@ public class Fibonacci {
         System.out.println("Введите желаемое количество чисел в ряду Фибоначчи");
         int str = scanner.nextInt();
         if (str < 0) { System.out.println("Введено отрицательное количество. Перезапустите программу и введите корректное значение"); return; }
-        long[] result = getFibonacciLine(str);
+        long[] result = getFibonacciLineNumber(str);
         for (int i=0; i<str; i++) {
             System.out.print(Long.toString(result[i]) + ", ");
+        }
+        System.out.println("...");
+
+        System.out.println("Введите максимально возможное значение для числа в ряду Фибоначчи");
+        int str1 = scanner.nextInt();
+        if (str1 < 0) { System.out.println("Введено отрицательное число. Перезапустите программу и введите корректное значение"); return; }
+        long[] result1 = getFibonacciLineValue(str1);
+        String s = "";
+        for (int i=0; result1[i] <= str1; i++) {
+            s = Long.toString(result1[i]) + ", ";
+            System.out.print(s);
         }
         System.out.println("...");
     }
@@ -18,7 +29,7 @@ public class Fibonacci {
     /*
     Метод позволяет получить массив чисел ряда Фибоначчи до заданного n-го по порядку числа
      */
-    private static long[] getFibonacciLine(int numberOfDigits) {
+    private static long[] getFibonacciLineNumber(int numberOfDigits) {
         long[] fibonacciLine = new long[numberOfDigits]; // объявление и выделение памяти под массив чисел ряда Фибоначчи
         //задаются значения для первых двух чисел ряда
         fibonacciLine[0] = 0;
@@ -26,6 +37,30 @@ public class Fibonacci {
         //определяются остальные числа вплоть до введенного n-го числа
         for (int i=2 ;i<numberOfDigits; i++) {
             fibonacciLine[i] = fibonacciLine[i-2] + fibonacciLine[i-1];
+        }
+        return fibonacciLine;
+    }
+
+    /*
+    Метод позволяет получить массив чисел ряда Фибоначчи до заданного максимально возможного значения числа
+     */
+    private static long[] getFibonacciLineValue(int valueOfDigits) {
+        long[] fibonacciLine = new long[2];
+        fibonacciLine[0] = 0;
+        fibonacciLine[1] = 1;
+        long temp = 0L; //временная переменная
+        long[] tempFibonacciLine = fibonacciLine; // копирование значений результирующего массива во временный
+        for(int i = 2; valueOfDigits >= temp; i++) {
+            fibonacciLine = new long[i+1]; // увеличивается размер результирующего массива
+            temp = tempFibonacciLine[i - 2] + tempFibonacciLine[i - 1]; // временная переменная хранит крайний на данный момент член ряда
+            // установка в результирующий массив (на данный момент все значения равны 0) всех известных на данный момент значений членов ряда Фибоначчи
+            int k = 1;
+            while ((i - k) > 0) {
+                fibonacciLine[i] = temp;
+                fibonacciLine[i-k] = tempFibonacciLine[i-k];
+                ++k;
+            }
+            tempFibonacciLine = fibonacciLine; //копирование значений членов ряда из результирующего во временный массив
         }
         return fibonacciLine;
     }
